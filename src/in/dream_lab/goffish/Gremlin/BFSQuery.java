247e8301c5da05305449e2815805eb13afe28edc
abhilashsharma <dx.abhilash@gmail.com>
2017-06-23 20:08:19 +0530

    Using StandardTitanGraph

--- src/in/dream_lab/goffish/Gremlin/BFSQuery.java
+++ src/in/dream_lab/goffish/Gremlin/BFSQuery.java
@@ -9,11 +9,13 @@ import com.tinkerpop.blueprints.Vertex;
 import com.tinkerpop.gremlin.java.GremlinPipeline; 
 import com.tinkerpop.pipes.PipeFunction; 
 import com.tinkerpop.pipes.branch.LoopPipe.LoopBundle; 
+import com.thinkaurelius.titan.graphdb.database.StandardTitanGraph;
+import org.apache.commons.configuration.BaseConfiguration;
 
 public class BFSQuery { 
   
-  private TitanGraph titanGraph = null; 
-   
+  private StandardTitanGraph titanGraph = null; 
+  private BaseConfiguration conf;
   public static void main(String args[]) { 
 //   GraphDatabase graph = new TitanGraphDatabase(); 
 //   graph.createGraphForMassiveLoad(GraphDatabaseBenchmark.TITANDB_PATH); 
@@ -21,27 +23,31 @@ public class BFSQuery {
 //   graph.shutdownMassiveGraph(); 
     
    BFSQuery titanQuery = new BFSQuery(); 
-   titanQuery.findNeighborsOfAllNodes();
+   titanQuery.TestQuery();
   } 
    
-  public BFSQuery(TitanGraph titanGraph) { 
+  
+  public BFSQuery(StandardTitanGraph titanGraph) { 
    this.titanGraph = titanGraph; 
   } 
    
   public BFSQuery() { 
-   this.titanGraph = TitanFactory.build() 
-     .set("storage.backend","cassandra") 
-     .set("storage.cassandra.keyspace","recreated") 
-     .set("storage.connection-timeout","5000000")
-     .set("storage.setup-wait","2000000")
-     .set("index.search.backend","elasticsearch")
-     .set("storage.hostname","10.0.0.12,10.0.0.13,10.0.0.14,10.0.0.15")
-     .set("index.search.hostname","10.0.0.12,10.0.0.13,10.0.0.14,10.0.0.15")
-     .set("cache.db-cache","true")
-     .open(); 
+    conf = new BaseConfiguration();
+    conf.setProperty("storage.backend","cassandra");
+    conf.setProperty("storage.cassandra.keyspace","recreated");
+    conf.setProperty("storage.connection-timeout","5000000");
+    conf.setProperty("storage.setup-wait","2000000");
+    conf.setProperty("index.search.backend","elasticsearch");
+    conf.setProperty("storage.hostname","10.0.0.12,10.0.0.13,10.0.0.14,10.0.0.15");
+    conf.setProperty("index.search.hostname","10.0.0.12,10.0.0.13,10.0.0.14,10.0.0.15");
+    conf.setProperty("cache.db-cache","true");
+    titanGraph = (StandardTitanGraph) TitanFactory.open(conf); 
   } 
    
  
+  public void TestQuery(){
+    
+  }
   @SuppressWarnings("unused") 
   public void findNeighborsOfAllNodes() { 
    for (Vertex v : titanGraph.getVertices()) { 
@@ -57,14 +63,21 @@ public class BFSQuery {
 
 
 /*AZURE TITAN CONFIGURATION
-conf = new BaseConfiguration()
-conf.setProperty("storage.backend","cassandra")
-conf.setProperty("storage.cassandra.keyspace","recreated")
-conf.setProperty("storage.connection-timeout","5000000")
-conf.setProperty("storage.setup-wait","2000000")
-conf.setProperty("index.search.backend","elasticsearch")
-conf.setProperty("storage.hostname","10.0.0.12,10.0.0.13,10.0.0.14,10.0.0.15")
-conf.setProperty("index.search.hostname","10.0.0.12,10.0.0.13,10.0.0.14,10.0.0.15")
-conf.setProperty("cache.db-cache","true")
-g = TitanFactory.open(conf)
+conf = new BaseConfiguration();
+conf.setProperty("storage.backend","cassandra");
+conf.setProperty("storage.cassandra.keyspace","recreated");
+conf.setProperty("storage.connection-timeout","5000000");
+conf.setProperty("storage.setup-wait","2000000");
+conf.setProperty("index.search.backend","elasticsearch");
+conf.setProperty("storage.hostname","10.0.0.12,10.0.0.13,10.0.0.14,10.0.0.15");
+conf.setProperty("index.search.hostname","10.0.0.12,10.0.0.13,10.0.0.14,10.0.0.15");
+conf.setProperty("cache.db-cache","true");
+g = TitanFactory.open(conf);
+
+*/
+
+
+/*
+ * titanGraph = TitanFactory.build().set("storage.backend","cassandra").set("storage.cassandra.keyspace","recreated").set("storage.connection-timeout","5000000").set("storage.setup-wait","2000000").set("index.search.backend","elasticsearch").set("storage.hostname","10.0.0.12,10.0.0.13,10.0.0.14,10.0.0.15").set("index.search.hostname","10.0.0.12,10.0.0.13,10.0.0.14,10.0.0.15").set("cache.db-cache","true").open(); 
+ */
 */
