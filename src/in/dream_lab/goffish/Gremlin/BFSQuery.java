ee4ec712ec456546a0297e4a527363a822ea5dd4
abhilashsharma <dx.abhilash@gmail.com>
2017-06-26 14:19:07 +0530

    Added BFS Query

--- src/in/dream_lab/goffish/Gremlin/BFSQuery.java
+++ src/in/dream_lab/goffish/Gremlin/BFSQuery.java
@@ -7,9 +7,16 @@ import com.tinkerpop.blueprints.Direction;
 import com.tinkerpop.blueprints.Edge; 
 import com.tinkerpop.blueprints.Vertex; 
 import com.tinkerpop.gremlin.java.GremlinPipeline; 
-import com.tinkerpop.pipes.PipeFunction; 
+import com.tinkerpop.pipes.PipeFunction;
+import com.tinkerpop.pipes.branch.LoopPipe;
 import com.tinkerpop.pipes.branch.LoopPipe.LoopBundle; 
 import com.thinkaurelius.titan.graphdb.database.StandardTitanGraph;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import org.apache.commons.configuration.BaseConfiguration;
 
 public class BFSQuery { 
@@ -24,7 +31,7 @@ public class BFSQuery {
 //   graph.shutdownMassiveGraph(); 
     
    BFSQuery titanQuery = new BFSQuery(); 
-   titanQuery.TestQuery();
+   titanQuery.bfsQuery("patid",4563712,3);
   } 
    
   
@@ -56,17 +63,110 @@ public class BFSQuery {
   }
   
   @SuppressWarnings("unused") 
-  public void findNeighborsOfAllNodes() { 
-   for (Vertex v : titanGraph.getVertices()) { 
-    for (Vertex vv : v.getVertices(Direction.BOTH, "similar")) { 
-    } 
-   } 
-  } 
+  public void bfsQuery(String key,Object value,int depth) {
+
+    HashSet<Object> visitedSet=new HashSet<>();
+    
+    PipeFunction<Vertex, Boolean> bfsFilterFunction = new PipeFunction<Vertex, Boolean>() {
+      public Boolean compute(Vertex i)
+      {
+          Object patid = i.getProperty("patid");
+          if(visitedSet.contains(i.getProperty("patid"))){
+              visitedSet.add(patid);
+              return true;
+          }
+          else
+              return false;
+      }
+
+  };
+    
+    
+    PipeFunction<LoopBundle<Vertex>,Boolean> whileFunction = new PipeFunction<LoopBundle<Vertex>,Boolean>(){
+
+      @Override
+      public Boolean compute(LoopBundle<Vertex> bundle) {
+       return bundle.getLoops()<= depth && bundle.getObject()!= null;
+      }
+
+     
+      
+    };
+    
+
+    PipeFunction<LoopBundle<Vertex>,Boolean> emitFunction = new PipeFunction<LoopBundle<Vertex>,Boolean>(){
+
+      @Override
+      public Boolean compute(LoopBundle<Vertex> bundle) {
+       return (bundle.getLoops()== depth+1) || (bundle.getLoops()< depth+1 && bundle.getObject().getEdges(Direction.OUT).iterator().hasNext()==false);
+      }
+
+     
+      
+    };
+    
+    
+    GremlinPipeline pipe =new GremlinPipeline(titanGraph).V(key, value).as("x").out().filter(bfsFilterFunction).loop("x", whileFunction,emitFunction).path();
+    
+    System.out.println(pipe.count());
+
+  }
+  
+  
+  public void shortestPath(){
+    final Vertex v1 = titanGraph.getVertex(2);
+    final Vertex v2 = titanGraph.getVertex(6);
+    final Set<Vertex> x = new HashSet<>(Collections.singleton(v1));
+
+    final GremlinPipeline<Object, List> pipe = new GremlinPipeline<>(v1).as("x")
+            .both().except(x).store(x).loop("x", new PipeFunction<LoopPipe.LoopBundle<Vertex>, Boolean>() {
+                @Override
+                public Boolean compute(LoopPipe.LoopBundle<Vertex> bundle) {
+                    return !x.contains(v2);
+                }
+            }, new PipeFunction<LoopPipe.LoopBundle<Vertex>, Boolean>() {
+                @Override
+                public Boolean compute(LoopPipe.LoopBundle<Vertex> bundle) {
+                    return bundle.getObject() == v2;
+                }
+            }).path();
+    for (final List path : pipe) {
+        System.out.println(path);
+    }
+    
+  }
+  
+  
+//  public void bfsQuery(String key, Object val){
+//    
+//    final Set<Vertex> x = new HashSet<>();
+//
+//    final GremlinPipeline<Object, List> pipe = new GremlinPipeline<>(titanGraph).V(key, val).as("x")
+//            .store(x).out().loop("x", new PipeFunction<LoopPipe.LoopBundle<Vertex>, Boolean>() {
+//                @Override
+//                public Boolean compute(LoopPipe.LoopBundle<Vertex> bundle) {
+//                    Vertex v =bundle.getObject();
+//                    v = Gremlin
+//                    
+//                }
+//            }, new PipeFunction<LoopPipe.LoopBundle<Vertex>, Boolean>() {
+//                @Override
+//                public Boolean compute(LoopPipe.LoopBundle<Vertex> bundle) {
+//                    return bundle.getObject() == v2;
+//                }
+//            }).path();
+//    for (final List path : pipe) {
+//        System.out.println(path);
+//    }
+//    
+//    
+//  }
    
 
 
    
  }
+
 
 
 /*AZURE TITAN CONFIGURATION
